53c86ce9bb74a5884c32516a1320e6919729dfd4
Vadim Rutkovsky <vrutkovs@redhat.com>
2017-01-21 17:16:35 +0100

    Try not to post existing entries and restart on exception

--- README.md
+++ README.md
@@ -18,3 +18,10 @@ password: <reddit user password>
   * `CHANNEL` - Telegram chat name (the bot has to be added as Administrator)
   
 * Run `python3 telegram_poster.py`
+
+## Notes
+
+The last sent submission id is being stored in `last_submission.id` file, so
+if this file exists the bot will skip all submissions in the previous ones until 
+this ID is found - or a new submission has been posted appears (this is determined 
+by UTC timestamp of the submission and bot start time)
--- telegram_poster.py
+++ telegram_poster.py
@@ -8,9 +8,21 @@ import praw
 import os
 import logging
 import html
+import sys
+
+from time import sleep
+from datetime import datetime
+
 
 log = logging.getLogger()
-log.setLevel(logging.INFO)
+log.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+log.addHandler(ch)
+
 
 if 'TOKEN' not in os.environ:
     raise RuntimeError("Put bot token in TOKEN env var")
@@ -24,28 +36,69 @@ if 'CHANNEL' not in os.environ:
 TOKEN = os.environ['TOKEN']
 SUBREDDIT = os.environ['SUBREDDIT']
 CHANNEL = os.environ['CHANNEL']
+START_TIME = datetime.utcnow().timestamp
+
+
+# Filter out old submissions here by reading a 'last_submission.id file'
+def read_last_submission_id():
+    try:
+        with open('last_submission.id', 'r') as f:
+            return f.read().strip()
+    except:
+        return None
+
+
+def write_last_submission_id(submission_id):
+    try:
+        with open('last_submission.id', 'w') as f:
+            f.write(submission_id)
+    except:
+        log.exception("Error writing submission ID")
+
+
+start_posting = False
+last_submission_id = read_last_submission_id()
+
+if not last_submission_id:
+    log.info("Last posted submission not found, starting with all submissions")
+    start_posting = True
+else:
+    log.info("Last posted submission is {}".format(last_submission_id))
 
 r = praw.Reddit(user_agent='Reddit Telegram poster by /u/roignac',
                 site_name="default")
 r.read_only = True
-
-bot = telegram.Bot(token=TOKEN)
 subreddit = r.subreddit(SUBREDDIT)
 
-for submission in subreddit.stream.submissions():
+bot = telegram.Bot(token=TOKEN)
+
+while True:
     try:
-        link = "https://redd.it/{id}".format(id=submission.id)
-        flair = html.escape(submission.link_flair_text or '')
-        title = html.escape(submission.title or '')
-        user = html.escape(submission.author.name or '')
+        for submission in subreddit.stream.submissions():
+            try:
+                link = "https://redd.it/{id}".format(id=submission.id)
+                if not start_posting and submission.created_utc < START_TIME:
+                    log.info("Skipping {} - last sent submission not yet found".format(
+                        submission.id))
+                    if submission.id == last_submission_id:
+                        start_posting = True
+                    continue
 
-        message_template = "<a href='{link}'>{title}</a> by <b>{user}</b> ({flair})"
-        if not flair:
-            message_template = "<a href='{link}'>{title}</a> by <b>{user}</b>"
+                flair = html.escape(submission.link_flair_text or '')
+                title = html.escape(submission.title or '')
+                user = html.escape(submission.author.name or '')
 
-        message = message_template.format(flair=flair, title=title, link=link, user=user)
+                message_template = "<a href='{link}'>{title}</a> by <b>{user}</b> ({flair})"
+                if not flair:
+                    message_template = "<a href='{link}'>{title}</a> by <b>{user}</b>"
 
-        log.info("Posting {}".format(message))
-        bot.sendMessage(chat_id=CHANNEL, parse_mode=telegram.ParseMode.HTML, text=message)
+                message = message_template.format(flair=flair, title=title, link=link, user=user)
+
+                log.info("Posting {}".format(link))
+                bot.sendMessage(chat_id=CHANNEL, parse_mode=telegram.ParseMode.HTML, text=message)
+                write_last_submission_id(submission.id)
+            except Exception as e:
+                log.exception("Error parsing {}".format(link))
     except Exception as e:
-        log.exception("Error parsing {}".format(link))
+        log.exception("Error fetching new submissions, restarting in 10 secs")
+        sleep(10)
